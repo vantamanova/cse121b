b5bf3f4081fc85b58d8ef911af5165491545c11e
vantamanova <vantamanova@yandex.ru>
2023-09-25 11:31:44 +0400

    W03: Operators

---
+++ practice/week03/operators.js
@@ -0,0 +1,71 @@
+// operators.js
+let shipHealth = 3;
+let shipAmmo = 3;
+let targetHealth = 3;
+
+function shipCanFire() {
+  // return true if the ships health is above 0 AND ammo is above 0
+  // false otherwise
+  if (shipHealth > 0 && shipAmmo > 0) {
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+function isHit() {
+  // should return true if a randomly generated number is greater
+  // than .5, false if it is less than .5
+  let hit = Math.random();
+  if (hit > 0.5) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function isDestroyed(health) {
+  // return true if health is zero or less
+  if (health <= 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function reloadShip() {
+  // reduce ship health by 1 and increase ammo by 3
+  shipHealth --;
+  shipAmmo += 3;
+}
+
+function fireShip() {
+  if (shipCanFire()) {
+    shipAmmo--;
+    if (isHit()) {
+      targetHealth--;
+      console.log("hit - targetHealth:", targetHealth);
+    } else {
+      console.log("miss");
+    }
+  } else {
+    reloadShip();
+    console.log("reloading, shipHealth:", shipHealth);
+  }
+}
+
+function encounter() {
+  console.log("You see a target");
+  if (!isDestroyed(targetHealth) && !isDestroyed(shipHealth)) {
+    fireShip();
+    if (isDestroyed(targetHealth)) {
+      console.log("Target eliminated");
+    }
+    if (isDestroyed(shipHealth)) {
+      console.log("ship destroyed");
+    }
+  }
+}
